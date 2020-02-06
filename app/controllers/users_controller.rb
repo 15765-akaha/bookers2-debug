b3d2cc954fdfb31acaa859e1d08358675bf82c69
15765-akaha <turning2012@gmail.com>
2020-02-06 12:11:13 +0000

    修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,9 +31,9 @@ class UsersController < ApplicationController
 
   #url直接防止　メソッドを自己定義してbefore_actionで発動。
    def baria_user
-  	unless params[:id].to_i == current_user.id
+  	if params[:id].to_i == current_user.id
   		redirect_to user_path(current_user)
   	end
    end
-
+ end
 end
