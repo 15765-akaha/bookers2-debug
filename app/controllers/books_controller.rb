95d8b5896dd5b9b6e9cb6ecdc5da8dcf7ba654bd
15765-akaha <turning2012@gmail.com>
2020-02-07 03:27:00 +0000

    userコントローラー修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,20 +1,26 @@
 class BooksController < ApplicationController
+   before_action :authenticate_user!
 
   def show
+    @book_new = Book.new
   	@book = Book.find(params[:id])
+    @user = User.find(@book.user_id)
   end
 
   def index
+    @book = Book.new
   	@books = Book.all #一覧表示するためにBookモデルの情報を全てくださいのall
   end
 
   def create
   	@book = Book.new(book_params) #Bookモデルのテーブルを使用しているのでbookコントローラで保存する。
+    @book.user_id = current_user.id
   	if @book.save #入力されたデータをdbに保存する。
-  		redirect_to @book, notice: "successfully created book!"#保存された場合の移動先を指定。
+  		flash[:notice] = "successfully created book!"
+      redirect_to book_path(@book) #保存された場合の移動先を指定。
   	else
   		@books = Book.all
-  		render 'index'
+  		render :index
   	end
   end
 
@@ -29,7 +35,7 @@ class BooksController < ApplicationController
   	if @book.update(book_params)
   		redirect_to @book, notice: "successfully updated book!"
   	else #if文でエラー発生時と正常時のリンク先を枝分かれにしている。
-  		render "edit"
+  		render :edit
   	end
   end
 
@@ -42,7 +48,7 @@ class BooksController < ApplicationController
   private
 
   def book_params
-  	params.require(:book).permit(:title)
+  	params.require(:book).permit(:title, :body)
   end
 
 end
